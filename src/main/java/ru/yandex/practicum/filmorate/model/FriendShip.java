969c88ed1ca35dbac9f36aad39705b63a42addc4
Artur Ryabov <B150B@yandex.ru>
2025-08-21 01:49:17 +0500

    Изменил модели для соответствия ТЗ

--- src/main/java/ru/yandex/practicum/filmorate/model/FriendShip.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FriendShip.java
@@ -1,2 +1,12 @@
-package ru.yandex.practicum.filmorate.model;public class FriendShip {
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class FriendShip {
+    private Long userId;
+    private Long friendId;
+    private boolean friendshipConfirmed;
 }
